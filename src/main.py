55164eb6c3398185d57157ba705dd95df75b595c
David Lichtenberg <david.m.lichtenberg@gmail.com>
2017-02-03 13:09:25 -0500

    can read files in path - lets pass them into our sorter

--- src/main.py
+++ src/main.py
@@ -1,14 +1,28 @@
 import argparse
+import os
+
+from os.path import isfile
 
 
 def main():
     parser = argparse.ArgumentParser(
         description='Print all unique lines from sorted files in a directory in sorted order'
     )
-    DIRECTORY = 'directory'
-    parser.add_argument(DIRECTORY, nargs='+', help='The relative path of the directory')
+    parser.add_argument('directory', help='The relative path of the directory')
     args = parser.parse_args()
-    path = args[DIRECTORY]
+    path = args.directory
+    files = files_in_path(path)
+
+    for file in files:
+        
+        print(open(file).readlines())
+
+
+def files_in_path(path):
+    for ff in os.listdir(path):
+        full_path = os.path.join(path, ff)
+        if isfile(full_path):
+            yield full_path
 
 
 if __name__ == '__main__':
