18b792e2fd04eab5eca5da9aee78f5773a63cb8f
David Lichtenberg <david.m.lichtenberg@gmail.com>
2017-02-03 15:16:24 -0500

    clean up combine.py

--- combine_sorted/combine.py
+++ combine_sorted/combine.py
@@ -1,4 +1,4 @@
-from typing import List, Generator, Sequence
+from typing import Generator, Sequence
 import heapq
 
 from combine_sorted.stream_queue import StreamQueue
@@ -11,11 +11,8 @@ def combine_sorted(streams: Sequence[StreamQueue]) -> Generator:
     heapq.heapify(heap)
 
     while len(heap) > 0:
-        ## TODO: if you have millions of files this check can become a bottle neck, but can be improved by storing peeked values in a min heap
         next_el, next_stream = heapq.heappop(heap)
         next_stream.pop()
-        # next_stream = get_next_stream(streams)
-        # next_el = next_stream.pop()
         if not next_stream.done:
             heapq.heappush(heap, (next_stream.peek(), next_stream))
 
@@ -24,9 +21,3 @@ def combine_sorted(streams: Sequence[StreamQueue]) -> Generator:
         if last_seen != next_el:
             last_seen = next_el
             yield next_el
-
-
-
-def get_next_stream(streams: List[StreamQueue]) -> StreamQueue:
-    lowest_next_el_index = min(range(len(streams)), key=lambda x: streams[x].peek())
-    return streams[lowest_next_el_index]
